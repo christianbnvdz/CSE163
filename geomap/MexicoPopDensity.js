4af1dfd1b5f1b62df43dda6d25b4044b9e4e69e7
Christian Benavidez <christianbenavidez57@gmail.com>
2020-05-19 09:37:43 -0700

    Added a legend for population density.

--- geomap/MexicoPopDensity.js
+++ geomap/MexicoPopDensity.js
@@ -36,13 +36,14 @@ d3.csv("mexico.csv").then(function(popData){
     }
   }
 
+  //Keep an array of the output color range
+  var colorRange = ["rgb(250, 250, 250)", "rgb(255, 180, 180)", 
+                    "rgb(255, 100, 100)", "rgb(255, 30, 30)", 
+                    "rgb(200, 0, 0)", "rgb(128, 0, 0)", "rgb(64, 0, 0)"];
   //Make a quantized scale for assigning fill color to states
   var colorScale = d3.scaleThreshold()
                      .domain([0, 50, 100, 250, 500, 750, 1000])
-                     .range(["rgb(255, 255, 255)", "rgb(255, 180, 180)",
-                             "rgb(255, 100, 100)", "rgb(255, 30, 30)",
-                             "rgb(200, 0, 0)", "rgb(128, 0, 0)", 
-                             "rgb(64, 0, 0)"]);
+                     .range(colorRange);
   //Create the projection
   var projection = d3.geoAlbers()
                      .rotate([102, 0])
@@ -50,7 +51,6 @@ d3.csv("mexico.csv").then(function(popData){
                      .parallels([17.5, 29.5])
                      .scale(1000)
                      .translate([width/2, height/2]);
-
   //Create a path generator
   var pathGenerator = d3.geoPath()
                         .projection(projection);
@@ -66,5 +66,59 @@ d3.csv("mexico.csv").then(function(popData){
             .attr("d", pathGenerator)
             .attr("fill", function(d){return colorScale(d.properties.value);});
 
+  //Create a grouping for the legend elements
+  var legendGroup = g.append("g");
+  //Create dimensions for legend container
+  var legendDim = {w: 175, h: 275};
+  //Create the x,y position for upper left part of the legend container
+  var legendPos = {x: width - margin.right - legendDim.w, 
+                   y: height - margin.bottom - legendDim.h};
+  //Add the containing legend rect
+  legendGroup.append("rect")
+             .attr("x", legendPos.x)
+             .attr("y", legendPos.y)
+             .attr("width", legendDim.w)
+             .attr("height", legendDim.h)
+             .attr("fill", "lightgrey");
+  //Add the 7 color boxes and labels that denote the density
+  var legendBoxes = legendGroup.append("g");
+  legendBoxes.selectAll("rect")
+             .data(colorRange)
+             .enter()
+             .append("rect")
+             .attr("x", legendPos.x + 145)
+             .attr("y", function(d, i){return legendPos.y + legendDim.h 
+                                              - 30 - (i * 30);})
+             .attr("width", 20)
+             .attr("height", 20)
+             .attr("fill", function(d){return d;});
+  //Add legend header
+  legendGroup.append("text")
+             .attr("x", legendPos.x + 20)
+             .attr("y", legendPos.y + 20)
+             .text("Population Density")
+             .attr("font-family", "sans-serif");
+  legendGroup.append("text")
+             .attr("x", legendPos.x + 37)
+             .attr("y", legendPos.y + 40)
+             .text("(Kilometers^2)")
+             .attr("font-family", "sans-serif");
+  //Create density label array
+  var densityLabels = ["No Data", "0 - 50 people per KM^2", 
+                       "50 - 100 people per KM^2", 
+                       "100 - 250 people per KM^2", "250 - 500 people per KM^2", 
+                       "500 - 750 people per KM^2", "750+ people per KM^2"];
+  //Add Density labels
+  var legendLabels = legendGroup.append("g");
+  legendLabels.selectAll("text")
+              .data(densityLabels)
+              .enter()
+              .append("text")
+              .attr("x", legendPos.x + 10)
+              .attr("y", function(d, i){return legendPos.y + legendDim.h
+                                               -16 - (i * 30)})
+              .text(function(d){return d;})
+              .attr("font-family", "sans-serif")
+              .attr("font-size", "10px");
 });
 });
